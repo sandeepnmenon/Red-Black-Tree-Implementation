3eab2699c9e92f1c67efcf945f1ef3d0fa57754a
Sandeep Menon <sandeep.n.menon@ieee.org>
2017-08-29 02:48:55 +0530

    Delete-BST added

--- main.cpp
+++ main.cpp
@@ -2,24 +2,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int main()
+RedBlackTree* createRedBlackTree(int nodeValues[],int n)
 {
-	int nodesToInsert[]={6,4,3,5,2};
-	int nodesToDelete[]={6,4,3,5,2};
-	
-	RedBlackTree *rbt = new RedBlackTree(1);
-	for(int i=0 ; i<sizeof(nodesToInsert)/sizeof(int); ++i)
+	RedBlackTree *rbt = new RedBlackTree();
+
+	for(int i=0 ; i < n; ++i)
 	{
-		rbt->insertNode(nodesToInsert[i]);
+		rbt->insertNode(nodeValues[i]);
 	}
 
-	cout<<"Inorder Traversal  ";
+	return rbt;
+}
+
+void printTreeTraversals(RedBlackTree *rbt,int index)
+{
+	cout<<"Inorder Traversal of RBT "<<index;
 	newline
 	rbt->inorderTraversal(rbt->getRoot());
 	newline
-	cout<<"Preorder Traversal ";
+	cout<<"Preorder Traversal of RBT "<<index;
 	newline
 	rbt->preorderTraversal(rbt->getRoot());
 	newline
+}
+int main()
+{
+	int nodesToInsert[]={1,2,3};
+	int nodesToDelete[]={6,4,3,5,2};
+	
+	RedBlackTree *rbt1 = createRedBlackTree(nodesToInsert,sizeof(nodesToInsert)/sizeof(int));
+
+	printTreeTraversals(rbt1,1);
+
+	//rbt1->deleteNode(2);
+	printTreeTraversals(rbt1,2);
+
+	int nodesToInsert2[]={4,5,6,7,8,9};
+	RedBlackTree *rbt2 = createRedBlackTree(nodesToInsert2,sizeof(nodesToInsert2)/sizeof(int));
+
+	printTreeTraversals(rbt2,3);
+
+	//merge(rbt1,rbt2);
+	printTreeTraversals(rbt2,4);
 }
